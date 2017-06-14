cd4ccc2674fa1f015f15c357f783c1272b57c0e2
伊藤永光 <g1644088@gmail.com>
2017-06-14 15:57:39 +0900

    動作確認の最終テストです 動きますように

---
+++ test_git-hub.java
@@ -0,0 +1,10 @@
+
+public class test_git-hub{
+    void run(String[] args){
+        System.out.println("月姫起動したぜ！！");
+    }
+    public static void main(String[] args){
+        test_git-hub test = new test();
+        test.run(args[0]);
+    }
+}
